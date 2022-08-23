b201a6ce8f5406cae01ae338926bb567a2b6e178
ChristianCursoIOS <cursoios2022@gmail.com>
2022-08-23 17:42:11 -0500

    Initial Commit

---
+++ Ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by user223728 on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
